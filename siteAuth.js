0ba27c31e79fb4ebd6e3038f93b16f75f050f07a
r2pen2 <joedobbelaar@gmail.com>
2023-07-31 19:45:29 -0400

    site auth

---
+++ siteAuth.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const router = express.Router();
+
+const db = require('../../firebase.js');
+
+/** Site text by ID, initialized to an empty dictionary */
+let sitePermissionsData = {}
+
+// On launch, fetch testimonial, offering, and staff data from Firebase
+const usersCollectionRef = db.collection("users");
+usersCollectionRef.onSnapshot((data) => {
+  console.log("Found updated users data");
+  sitePermissionsData = {}; // Clear data
+  for (const doc of data.docs) {
+      sitePermissionsData[doc.id] = doc.data();
+  }
+})
+
+router.get('/' , (req, res) => {
+  const userPermissions = sitePermissionsData[req.query.id];
+  if (userPermissions) {
+    if (userPermissions.op) {
+      for (const key of Object.keys(userPermissions)) {
+        userPermissions[key] = true;
+      }
+    }
+    res.json(userPermissions);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+router.post("/", (req, res) => {
+  const permissions = req.body.permissions;
+  const docRef = db.doc(`users/${req.body.userId}`);
+  const newUserData = {};
+  for (const permission of Object.values(permissions)) {
+    newUserData[permission] = false;
+  }
+  docRef.set(newUserData).then(() => {
+    res.sendStatus(200);
+  });
+})
+
+module.exports = router;
