25d69a6345a26299d41560e1e8ff998b9ad3e42a
Joe Dobbelaar <joedobbelaar@gmail.com>
2023-09-08 13:54:01 -0400

    mailmanager update

--- siteMail.js
+++ siteMail.js
@@ -1,29 +1,45 @@
 const express = require('express');
-const router = express.Router();
 
 const nodemailer = require('nodemailer');
 
-router.post("/", (req, res) => {
-  var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: req.body.fromAddress,
-      pass: req.body.fromPassword
-    }
-  });
-  var mailOptions = {
-    from: req.body.fromAddress,
-    to: req.body.toAddress,
-    subject: req.body.subject,
-    text: req.body.text
-  };
-  transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Email sent: ' + info.response);
-    }
-  });
-})
 
-module.exports = router;
+class SiteMailManager {
+
+  constructor(fromAddress, fromPassword) {
+    this.password = fromPassword;
+    this.email = fromAddress;
+  }
+
+  initialize() {
+    this.router = express.Router();
+    this.router.post("/", (req, res) => {
+      var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: this.email,
+          pass: this.password
+        }
+      });
+      var mailOptions = {
+        from: this.email,
+        to: req.body.toAddress,
+        subject: req.body.subject,
+        text: req.body.text
+      };
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+    })
+  }
+  
+  getRouter() {
+    this.initialize();
+    return this.router;
+  }
+}
+
+module.exports = SiteMailManager;
