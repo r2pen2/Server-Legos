0ff00e1e135a5d25d4676aec7149a687ea469be0
r2pen2 <joedobbelaar@gmail.com>
2023-09-10 21:28:28 -0400

    added siteforms

---
+++ siteForms.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+
+const db = require('../../firebase.js');
+
+/** Site forms by ID, initialized to an empty dictionary */
+let siteFormsData = {}
+
+// On launch, fetch all forms from Firebase
+const siteFormsCollectionRef = db.collection("siteForms");
+siteFormsCollectionRef.onSnapshot((data) => {
+  console.log("Found updated siteForms data");
+  siteFormsData = {}; // Clear data
+  for (const doc of data.docs) {
+      const data = doc.data();
+      siteFormsData[doc.id] = data.text;
+  }
+})
+
+router.post("/", (req, res) => {
+  if (req.body.action) {
+    if (req.body.action === "delete") {
+      const docRef = db.doc(`${"siteForms"}/${req.body.documentId}`);
+      docRef.delete().then(() => {
+        res.sendStatus(200);
+      })
+    }
+    if (req.body.action === "create") {
+      db.collection("siteForms").add(req.body.documentData).then(() => {
+        res.sendStatus(200);
+      })
+    }
+  } else {
+    const docRef = db.doc(`${"siteForms"}/${req.body.documentId}`);
+    docRef.set(req.body.documentData).then(() => {
+      res.sendStatus(200);
+    });
+  }
+})
+
+module.exports = router;
