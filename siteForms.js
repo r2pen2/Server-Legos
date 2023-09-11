0519f94eb03823413b2b2ff32532277473b19493
r2pen2 <joedobbelaar@gmail.com>
2023-09-11 13:00:27 -0400

    siteforms no console.log

--- siteForms.js
+++ siteForms.js
@@ -27,12 +27,9 @@ class SiteFormManager {
     this.router = express.Router();
     this.router.get('/' , (req, res) => {
       const key = req.query.key;
-      console.log(key)
       if (key !== this.formKey || !this.formKey) {
-        console.log("Invalid form key received.")
         res.send(400)
       } else {
-        console.log("Form key is valid!")
         res.json(siteFormsData);
       }
     });
